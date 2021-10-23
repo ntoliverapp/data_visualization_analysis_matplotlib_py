852857bb9c444268191022bc32d8486fcb157a35
ntoliverapp <n.toliverapplication@gmail.com>
2021-10-23 12:39:25 -0700

    updated

--- pie_charts/nba_team_pop.py
+++ pie_charts/nba_team_pop.py
@@ -3,16 +3,17 @@ import matplotlib.pyplot as plt
 plt.style.use('ggplot')
 
 # Team Popularity
-labels = ['Lakers', 'Warriors', 'Nets', 'Celtics', 'Suns', ]
-
+labels = ['Lakers', 'Warriors', 'Hawks', 'Celtics', 'Suns', ]
 popularity = [59219, 55466, 36443, 35917, 31991]
+colors= ['#FDB927', '#5D76A9', '#E03A3E', '#007A33', '#E56020']
+explode= [0.1, 0, 0, 0, 0]
 
-colors= ['#552583', '#1D428A', 'black', '#007A33', '#E56020']
-
-plt.pie(popularity, labels=labels, colors=colors, wedgeprops={'edgecolor':'black'})
+plt.pie(popularity, labels=labels, colors=colors, explode=explode, autopct='%1.1f%%')
 
 plt.title('NBA Teams')
 
 plt.tight_layout()
+
+plt.
 
 plt.show()
